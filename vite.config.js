c5848d13281bb80abba7f0fdfc18cc4d7096aa0f
nayuxuohz <nayuxuohz@nayuxuohzdeMacBook-Pro.local>
2021-07-05 12:12:25 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  base: './',
+  // base: './',
   plugins: [reactRefresh()],
   server: {
     proxy: {
